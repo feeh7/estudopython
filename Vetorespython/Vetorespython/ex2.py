5aabe07d4f7a524839f0e461591475fefdfef4ae
Rafael Felix De Maria <rafael.maria01@etec.sp.gov.br>
2024-09-09 11:23:13 -0300

    Delete Vetorespython/Vetorespython directory

--- Vetorespython/Vetorespython/ex2.py
+++
@@ -1,9 +0,0 @@
-maioremenor = [] #vetor
-
-print("Olá, digite 10 números e irei mostrando-os para você todos eles do menor para o maior") #mensagem ao usuario
- 
-for i in range(10): #repetição da pergunta abaixo
-    numero = float(input(f"Digite o número {i + 1}: "))
-    maioremenor.append(numero)
-    maioremenor.sort() #.sort(ordena do menor para maior)
-    print("Números do menor para o maior:", maioremenor)
